8c99e9322667b7312986ee41041fff7d5f8f7de2
zwen <zwen000@citymail.cuny.edu>
2020-08-31 00:36:12 -0400

    first commit

---
+++ app/src/main/java/wen/learnprogramming/youtubeplayer/MainActivity.kt
@@ -0,0 +1,11 @@
+package wen.learnprogramming.youtubeplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
